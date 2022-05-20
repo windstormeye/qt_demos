f33857bf81f5e54b1fe43824d67f7d7298b5c6bd
windstormeye <877302410@qq.com>
2022-05-21 00:49:34 +0800

    [image_editor]: init project & import easyExif tool

---
+++ image_editor/main.cpp
@@ -0,0 +1,22 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include "Utils/Exif/exif.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(u"qrc:/qmls/Qmls/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    easyexif::EXIFInfo info;
+
+    return app.exec();
+}
